6025f3ab789d537ebcadd4c5746ee8a8eb98f8db
Marvin Buss <marvin.buss@gmail.com>
2024-05-27 14:54:56 +0200

    Fix bug in TF

--- code/infra/datafactory_content.tf
+++ code/infra/datafactory_content.tf
@@ -11,35 +11,27 @@ resource "azurerm_resource_group_template_deployment" "data_factory_content_depl
   template_content   = file(var.data_factory_published_content.template_file)
 }
 
-resource "time_sleep" "sleep_data_factory_content_deployment" {
-  create_duration = "60s"
-
-  depends_on = [
-    azurerm_resource_group_template_deployment.data_factory_content_deployment
-  ]
-}
-
 resource "null_resource" "data_factory_triggers_start" {
-  for_each = toset(var.data_factory_triggers_start)
+  for_each = var.data_factory_published_content.parameters_file != "" && var.data_factory_published_content.template_file != "" ? toset(var.data_factory_triggers_start) : {}
 
   provisioner "local-exec" {
     command = "az datafactory trigger start --resource-group ${azurerm_data_factory.data_factory.resource_group_name} --factory-name ${azurerm_data_factory.data_factory.name} --name ${each.value}"
   }
 
   depends_on = [
-    time_sleep.sleep_data_factory_content_deployment
+    azurerm_resource_group_template_deployment.data_factory_content_deployment
   ]
 }
 
 resource "null_resource" "data_factory_pipelines_run" {
-  for_each = toset(var.data_factory_pipelines_run)
+  for_each = var.data_factory_published_content.parameters_file != "" && var.data_factory_published_content.template_file != "" ? toset(var.data_factory_pipelines_run) : {}
 
   provisioner "local-exec" {
     command = "az datafactory pipeline create-run --resource-group ${azurerm_data_factory.data_factory.resource_group_name} --factory-name ${azurerm_data_factory.data_factory.name} --name ${each.value}"
   }
 
   depends_on = [
-    time_sleep.sleep_data_factory_content_deployment,
+    azurerm_resource_group_template_deployment.data_factory_content_deployment,
     azurerm_kusto_script.kusto_script,
   ]
 }
