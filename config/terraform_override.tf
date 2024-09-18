0e68e9ea028a4818d8aa96a42ce017cbbb5c5fdf
Marvin Buss <marvin.buss@gmail.com>
2024-09-19 00:18:28 +0200

    Update tf version

--- config/terraform_override.tf
+++ config/terraform_override.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.103.1"
+      version = "4.2.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.11.1"
+      version = "0.12.1"
     }
     null = {
       source  = "hashicorp/null"
-      version = "3.2.2"
+      version = "3.2.3"
     }
   }
 }
